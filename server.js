6c149c9d89a79fcfa627bad7dc014f5f7e38d14c
Kushagra Gupta <guptakushagra99@gmail.com>
2025-11-15 22:28:13 +0530

    updated code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js App!");
+  res.send("Hello from Kushagra Gupta!");
 });
 
 app.get("/health", (req, res) => {
